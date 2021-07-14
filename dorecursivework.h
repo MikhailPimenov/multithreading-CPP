92424bbdb9cbf371cb28d68195e29199fd377719
MikhailPimenov <misha4git@yandex.ru>
2021-07-14 22:33:41 +0300

    part-9: std::recursive_mutex

---
+++ dorecursivework.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+void doRecursiveWork(int a);
--- main.cpp
+++ main.cpp
@@ -2,31 +2,15 @@
 #include <iostream>
 #include <thread>
 
-#include "dowork.h"
-#include "Timer.h"
+#include "dorecursivework.h"
 
 int main() {
-	Timer timer;
 
-	std::thread thread1(doWork3, '*');
-	std::thread thread2(doWork4, '#');
+	std::thread thread1(doRecursiveWork, 10);
+	std::thread thread2(doRecursiveWork, 10);
 
 	thread1.join();
 	thread2.join();
-
-	double time = timer.elapsed();
-	std::cout << "time = " << time << std::endl;
-
-	
-	timer.reset();
-	std::thread thread3(doWork_with_deadlock, '*');
-	std::thread thread4(doWork_with_deadlock2, '#');
-
-	thread3.join();
-	thread4.join();
-
-	time = timer.elapsed();
-	std::cout << "time = " << time << std::endl;
 
 	return 0;
 }
