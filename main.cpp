1898210d534d52d062c71b82b5ce338708bef803
MikhailPimenov <misha4git@yandex.ru>
2021-07-14 11:46:06 +0300

    part-4: getting the result from thread using lambda

--- main.cpp
+++ main.cpp
@@ -2,25 +2,20 @@
 #include <iostream>
 #include <thread>
 
-#include "dowork.h"
-
+#include "sum.h"
 
 int main() {
 	int result = 5;
 	std::cout << result << std::endl;
 
-	std::thread thread1(doWork, std::ref(result));
+	std::thread thread1([&result](){ result = sum(7, 12); });
 	
 	for (int i = 0; i < 10; ++i) {
 		std::cout << std::this_thread::get_id() << " main() " << i << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(250));
 	}
-
-
-	thread1.join();            // stop and wait here unless thread1 finishes
-	//thread1.detach();        // in case of using detach() it is important 
-	                           // to call detach() after result changes in doWork
 	
+	thread1.join();
 	std::cout << result << std::endl;
 
 	return 0;
---
+++ sum.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+int sum(int a, int b);
